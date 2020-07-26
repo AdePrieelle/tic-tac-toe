3d02792d1fc6e3387c073d05208e6982c16f297e
Antoine de Prieelle <antoine_@live.nl>
2020-07-26 18:21:10 +0200

    Add addmark function and decidefirstmove function

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,6 +14,11 @@ const startGamePlay = document.querySelector(".start-game-play");
 const gameboardGrid = document.querySelector(".gameboard-grid");
 const playGameButtons = document.querySelector(".play-game-buttons");
 const startGame = document.querySelector(".start-game");
+const startGameEnd = document.querySelector(".start-game-end");
+const resetGame = document.querySelector(".reset-game");
+const resetGameEnd = document.querySelector(".reset-game-end");
+const resetGameRoundRedo = document.querySelector(".reset-game-round-redo");
+const resetGameRoundNext = document.querySelector(".reset-game-round-next");
 const playersScore = document.querySelector(".players-score");
 
 playAgainstPlayer.addEventListener("click", function() {
@@ -41,6 +46,12 @@ startGamePlay.addEventListener("click", function() {
   gameboardGrid.setAttribute("style", "display: grid");
   playGameButtons.setAttribute("style", "display: flex");
   playersScore.setAttribute("style", "display: flex");
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
 });
 
 startGame.addEventListener("click", function() {
@@ -54,6 +65,69 @@ startGame.addEventListener("click", function() {
   gameboardGrid.setAttribute("style", "display: none");
   playGameButtons.setAttribute("style", "display: none");
   playersScore.setAttribute("style", "display: none");
+  // reset gameBoard
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
+});
+
+startGameEnd.addEventListener("click", function() {
+  playAgainst.setAttribute("style", "display: flex;");
+  playAgainstPlayer.setAttribute("style", "opacity: 1;");
+  playAgainstComputer.setAttribute("style", "opacity: .5;");
+  labelPlayer2.setAttribute("style", "display: flex");
+  inputPlayer2.setAttribute("style", "display: flex");
+  formPlayers.setAttribute("style", "display: flex;");
+  startGameButton.setAttribute("style", "display: flex;");
+  gameboardGrid.setAttribute("style", "display: none");
+  playGameButtons.setAttribute("style", "display: none");
+  playersScore.setAttribute("style", "display: none");
+  // reset gameBoard
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
+});
+
+resetGameRoundRedo.addEventListener("click", function() {
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
+});
+
+resetGameRoundNext.addEventListener("click", function() {
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
+});
+
+resetGame.addEventListener("click", function() {
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
+});
+
+resetGameEnd.addEventListener("click", function() {
+  gameBoard.gameBoardArray = [
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
+  ];
+  displayController.render(gameBoard.gameBoardArray);
 });
 
 
@@ -61,9 +135,9 @@ startGame.addEventListener("click", function() {
 
 const gameBoard = (() => {
   let gameBoardArray = [
-    ["X", "", "O"], 
-    ["O", "O", ""], 
-    ["X", "X", "X"]
+    ["", "", ""], 
+    ["", "", ""], 
+    ["", "", ""]
   ];
   
   return {
@@ -74,16 +148,18 @@ const gameBoard = (() => {
 // Step 3
 
 const displayController = (() => {
+
+  const item0 = document.querySelector(".gridboard-item-0-0");
+  const item1 = document.querySelector(".gridboard-item-0-1");
+  const item2 = document.querySelector(".gridboard-item-0-2");
+  const item3 = document.querySelector(".gridboard-item-1-0");
+  const item4 = document.querySelector(".gridboard-item-1-1");
+  const item5 = document.querySelector(".gridboard-item-1-2");
+  const item6 = document.querySelector(".gridboard-item-2-0");
+  const item7 = document.querySelector(".gridboard-item-2-1");
+  const item8 = document.querySelector(".gridboard-item-2-2");
+  
   const render = function(gameBoardArray) {
-    const item0 = document.querySelector(".gridboard-item-0-0");
-    const item1 = document.querySelector(".gridboard-item-0-1");
-    const item2 = document.querySelector(".gridboard-item-0-2");
-    const item3 = document.querySelector(".gridboard-item-1-0");
-    const item4 = document.querySelector(".gridboard-item-1-1");
-    const item5 = document.querySelector(".gridboard-item-1-2");
-    const item6 = document.querySelector(".gridboard-item-2-0");
-    const item7 = document.querySelector(".gridboard-item-2-1");
-    const item8 = document.querySelector(".gridboard-item-2-2");
 
     item0.innerHTML = `${gameBoardArray[0][0]}`;
     item1.innerHTML = `${gameBoardArray[0][1]}`;
@@ -96,58 +172,209 @@ const displayController = (() => {
     item8.innerHTML = `${gameBoardArray[2][2]}`;
   };
 
+  // Step 4
+  const addMark = function() {
+   
+    let marker = "";
+    const changeMarker = function() {
+      if (player1.isTurn == true) {
+        marker = player1.playerMarker;
+        player1.isTurn = false;
+        player2.isTurn = true;
+      } else {
+        marker = player2.playerMarker;
+        player2.isTurn = false;
+        player1.isTurn = true;
+      }
+    }
+
+    item0.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[0][0] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[0][0] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item1.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[0][1] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[0][1] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item2.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[0][2] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[0][2] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+  
+    item3.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[1][0] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[1][0] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item4.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[1][1] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[1][1] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item5.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[1][2] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[1][2] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item6.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[2][0] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[2][0] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item7.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[2][1] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[2][1] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+    item8.addEventListener("click", function() {
+      if (gameBoard.gameBoardArray[2][2] === "") {
+        changeMarker();
+        gameBoard.gameBoardArray[2][2] = marker;
+        displayController.render(gameBoard.gameBoardArray);
+      } else {
+        return;
+      }
+    });
+
+  };
+
+  const decideFirstMove = function() {
+
+    startGamePlay.addEventListener("click", function() {
+      player1.isFirstMove = true;
+      player2.isFirstMove = false;
+      player1.isTurn = true;
+      player2.isTurn = false;
+    });
+
+    startGame.addEventListener("click", function() {
+      player1.isFirstMove = true;
+      player2.isFirstMove = false;
+      player1.isTurn = true;
+      player2.isTurn = false;
+    });
+
+    startGameEnd.addEventListener("click", function() {
+      player1.isFirstMove = true;
+      player2.isFirstMove = false;
+      player1.isTurn = true;
+      player2.isTurn = false;
+    });
+
+    resetGame.addEventListener("click", function() {
+      player1.isFirstMove = true;
+      player2.isFirstMove = false;
+      player1.isTurn = true;
+      player2.isTurn = false;
+    });
+
+    resetGameEnd.addEventListener("click", function() {
+      player1.isFirstMove = true;
+      player2.isFirstMove = false;
+      player1.isTurn = true;
+      player2.isTurn = false;
+    });
+
+    resetGameRoundRedo.addEventListener("click", function() {
+      player1.isTurn = player1.isFirstMove;
+      player2.isTurn = player2.isFirstMove;
+    });
+
+    resetGameRoundNext.addEventListener("click", function() {
+      if (player1.isFirstMove === true) {
+        player1.isFirstMove = false;
+        player1.isTurn = false;
+        player2.isFirstMove = true;
+        player2.isTurn = true;
+      } else {
+        player1.isFirstMove = true;
+        player1.isTurn = true;
+        player2.isFirstMove = false;
+        player2.isTurn = false;
+      };
+    });
+
+  };
+
   return {
-    render
+    render,
+    decideFirstMove,
+    addMark
   };
 })();
 
-const player = (name) => {
+
+// Step 4
+const player = (name, marker, isPlayerTurn, isPlayerFirstMove) => {
+  let playerMarker = marker;
+  let isTurn = isPlayerTurn;
+  let isFirstMove = isPlayerFirstMove;
   return {
-    name
+    name,
+    isTurn,
+    isFirstMove,
+    playerMarker
   };
 };
 
+const player1 = player("player1", "X", true, true);
+const player2 = player("player2", "O", false, false);
+
 displayController.render(gameBoard.gameBoardArray);
-
-
-// Step 4
+displayController.decideFirstMove();
+displayController.addMark();
 
 
 
 /*
-4.  Build the functions that allow players to add marks to a specific spot on the board, 
-    and then tie it to the DOM, letting players click on the gameboard to place their marker. 
-    Don’t forget the logic that keeps players from playing in spots that are already taken! 
+5.  Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
 
-    4.1 Think carefully about where each bit of logic should reside. 
-        Each little piece of functionality should be able to fit 
-        in the game, player or gameboard objects.. 
-        but take care to put them in “logical” places. 
-        Spending a little time brainstorming here can make your life much easier later!
-
-          - player
-              - isTurn = true;
-
-          - displayController
-              - let marker = "";
-              - if (innerHTML of document.queryselector(".class") is empty )
-                - Make a function addMark() that will change innerHTML based on 
-                  - if (playerX.isTurn) change to "X") {
-                    marker = "X"
-                    playerX.isTurn = false;
-                    playerO.isTurn = true;
-                  } else {
-                    marker = "O"
-                    playerX.isTurn = true;
-                    playerO.isTurn = false;
-                  }
-
-              - update the gameboard array with marker where is clicked
-                  - tile1.addeventlistener("click", add marker to gameBoard array location)
-                  - tile2.adaddeventlistener("click", add marker to gameBoard array location)
-                  - ....
-
-              - render the gameboard from the gameBoard array
+      - if (check gameBoard array for 3 in a row)
+        - player object
+            -isWinner = true;
+            update player.score
 */
 
 
@@ -228,6 +455,7 @@ Steps
       - if (check gameBoard array for 3 in a row)
         - player object
             -isWinner = true;
+            update player.score
 
 
 6.  Clean up the interface to allow players to put in their names, 
